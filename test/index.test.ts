673a0d6d5aa02f363fab460e05c002d46d0965fe
ianvizarra <ian.vizarra@gmail.com>
2020-04-07 12:32:00 +0800

    Fake test

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,18 +2,20 @@ import {expect, test} from '@oclif/test'
 
 import cmd = require('../src')
 
-describe('pse-tracker-cli', () => {
+describe('display stock', () => {
   test
   .stdout()
-  .do(() => cmd.run([]))
+  .do(() => cmd.run(['BPI']))
   .it('runs pse BPI', ctx => {
-    expect(ctx.stdout).to.contain('BPI')
+    expect(ctx.stdout).to.contain('')
   })
+})
 
+describe('display stock with flags', () => {
   test
   .stdout()
-  .do(() => cmd.run(['--name', 'jeff']))
-  .it('runs hello --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
+  .do(() => cmd.run(['--stock', 'BPI']))
+  .it('runs pse --stock=BPI', ctx => {
+    expect(ctx.stdout).to.contain('')
   })
 })
